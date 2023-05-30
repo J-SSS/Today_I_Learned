f74a7903841a4d513e2e261038a9ec4ed970ad63
J-SSS <dongr5102@naver.com>
2023-05-30 21:29:07 +0900

    0530

---
+++ 00_BACKJOON/src/step_by_step/s06_심화1/Quiz2444.java
@@ -0,0 +1,28 @@
+package step_by_step.s06_심화1;
+
+
+import java.util.*;
+
+public class Quiz2444 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+
+        for(int i = 1; i <= cnt ; i++) {
+            for(int j = 0; j < cnt-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for(int i = cnt-1; i >= 0 ; i--) {
+            for(int j = 0; j < cnt-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
