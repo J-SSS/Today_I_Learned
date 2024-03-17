f55ea7a47acfa2f4cf2aeb2a5c3c9fbb2a674a1c
Dongil Jang <dongr5102@naver.com>
2024-03-18 08:43:31 +0900

    [Bronze IV] Title: 베라의 패션, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+
+        System.out.println(cnt * (cnt-1));
+    }
+}
