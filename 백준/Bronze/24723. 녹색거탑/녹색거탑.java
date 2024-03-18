00975b88e12b8574dd48d5909b6b05e56b1ec551
Dongil Jang <dongr5102@naver.com>
2024-03-18 09:32:07 +0900

    [Bronze IV] Title: 녹색거탑, Time: 120 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        System.out.println((int)Math.pow(2,cnt));
+    }
+}
