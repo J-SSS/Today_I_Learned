1893b442fb5d090cc266366885ece43ba1dc5ed4
Dongil Jang <dongr5102@naver.com>
2024-05-20 13:28:04 +0900

    [Silver III] Title: N과 M (4), Time: 796 ms, Memory: 26348 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static boolean[] visitFlag;
+    static int[] output;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 1~N까지
+        int M = Integer.parseInt(st.nextToken()); // M개 선택(깊이)
+
+        visitFlag = new boolean[N];
+        output = new int[M];
+
+        dfs(N,M,0);
+    }
+
+    static void dfs(int N, int M, int D){
+
+        if(D == M){
+            for (int i : output) {
+                System.out.print(i+" ");
+            }
+            System.out.println("");
+            return;
+        }
+
+        for(int i = 0 ; i < N; i++){
+                if(D==0){
+                    output[D] = i+1;
+                    dfs(N,M,D+1);
+                } else if(output[D-1]<=i+1) {
+                    output[D] = i+1;
+                    dfs(N,M,D+1);
+                }
+        }
+    }
+}
