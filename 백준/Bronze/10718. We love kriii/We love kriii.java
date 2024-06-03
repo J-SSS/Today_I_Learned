42d1ab8ed4be7e82cc649c8cf45045cd984b56fe
Dongil Jang <dongr5102@naver.com>
2024-06-03 10:15:28 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13824 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}
