39496505fd2d051c2ac83fd017ea54ce1e031c14
Dongil Jang <dongr5102@naver.com>
2024-06-03 10:04:09 +0900

    [Bronze V] Title: Rook, Time: 104 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(
+                "  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\"
+        );
+    }
+}
