af7d71054d6cebebc7b7b8969184ad2284b038bc
Dongil Jang <dongr5102@naver.com>
2024-06-04 09:21:34 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 104 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
