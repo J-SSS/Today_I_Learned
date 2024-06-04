16eebb5a659b6b469adcd1db83beae67d6661860
Dongil Jang <dongr5102@naver.com>
2024-06-04 10:38:12 +0900

    [Bronze V] Title: 등록, Time: 120 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("162\n" +
+                "dongr5102");
+    }
+}
