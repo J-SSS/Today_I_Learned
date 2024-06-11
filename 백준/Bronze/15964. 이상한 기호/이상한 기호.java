69b15a432d25bc2fb2f21abc0545fc5144470fd0
Dongil Jang <dongr5102@naver.com>
2024-06-11 09:34:51 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14160 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        System.out.println((A+B)*(A-B));
+    }
+}
