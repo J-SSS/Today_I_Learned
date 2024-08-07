1d55fdf6bea405cfe110f51108a4905343172f1c
Dongil Jang <dongr5102@naver.com>
2024-08-07 09:56:05 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14476 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,17 +1,19 @@
+import java.math.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int length = Integer.parseInt(br.readLine());
         String input = br.readLine();
-        long rslt = 0;
+        BigInteger rslt = new BigInteger("0");
 
         for (int i = 0; i < length; i++) {
             int num = input.charAt(i) - 96;
-            rslt += num * (int) Math.pow(31,i);
+            rslt = rslt.add(BigInteger.valueOf(num).multiply(BigInteger.valueOf(31).pow(i)));
         }
 
-        System.out.println(rslt);
+        rslt = rslt.remainder(BigInteger.valueOf(1234567891));
 
+        System.out.println(rslt);
     }
 }
