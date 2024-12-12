8abcbfeaf8f4f7b200869dcb55ddcc2fef28d5a9
Dongil Jang <dongr5102@naver.com>
2024-12-12 17:02:20 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 7.54 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length() % 2 == 0) answer = s.substring(s.length()/2-1, s.length()/2+1);
+        else answer = s.charAt(s.length()/2) + "";
+        
+        return answer;
+    }
+}
