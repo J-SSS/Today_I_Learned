18166f3ea0aceefa87ae5ab7353851d842739273
Dongil Jang <dongr5102@naver.com>
2024-12-12 10:19:52 +0900

    [level 0] Title: 안전지대, Time: 0.06 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,15 +1,18 @@
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
+        int n = board.length;
+        
         int[] dx = {0,1,-1,0,-1,1,1,-1};
         int[] dy = {1,0,0,-1,-1,1,-1,1};
-        for(int i = 0 ; i < board.length ; i++){
+        
+        for(int i = 0 ; i < n ; i++){
             inner:
-            for(int j = 0 ; j < board[i].length ; j++){
+            for(int j = 0 ; j < n ; j++){
                 if(board[i][j] == 0) {
                     for(int k = 0 ; k < 8 ; k++){
-                        if(i+dx[k] > -1 && i+dx[k] < board.length
-                           && j+dy[k] > -1 && j+dy[k] < board[i].length
+                        if(i+dx[k] > -1 && i+dx[k] < n // x좌표 검사
+                           && j+dy[k] > -1 && j+dy[k] < n // y좌표 검사
                            && board[i+dx[k]][j+dy[k]] == 1){
                             continue inner;
                         }
