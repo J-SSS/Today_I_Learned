3d5f51e6ea4c46124ae24200feefef1555edb2a6
Dongil Jang <dongr5102@naver.com>
2024-12-17 13:27:53 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.63 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        int[] answer = set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
